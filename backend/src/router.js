541c642a12fba632542b3bfbdd448bb8ed35d259
Jonas B. Franco <jonasbfranco@hotmail.com>
2023-03-17 16:36:12 -0300

    🐋 - Dockerfile and DockerCompose

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,6 +5,15 @@ const router = express.Router();
 const imoveisController = require('./controllers/imoveisController');
 const imoveisMiddleware = require('./middlewares/imoveisMiddleware');
 
+router.get('/', (req, res) => {
+    const teste = [
+        {
+            "status": "App funcionando 2",
+        }
+    ];
+    return res.status(200).json(teste);
+});
+
 router.get('/imoveis', imoveisController.getAll);
 router.post('/imoveis', imoveisMiddleware.validateFieldsPost, imoveisController.createImovel);
 router.delete('/imoveis/:id', imoveisController.deleteImovel);
