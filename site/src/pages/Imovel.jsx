cf24f9b1b9f2b4d0c89c5961bd76601dbca452e1
Jonas B. Franco <jonasbfranco@hotmail.com>
2023-03-24 13:31:03 -0300

    📌 Final css config

--- site/src/pages/Imovel.jsx
+++ site/src/pages/Imovel.jsx
@@ -37,50 +37,44 @@ const Imovel = () => {
       {imovel &&  (
       <>
         <ImovelCard imovel={imovel} showLink={false} />
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Endereço: 
           </h3>
-          <p>{imovel[0].endereco_imovel}</p>
+          <p>{imovel[0].endereco_imovel}, nº {imovel[0].numero_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
-            <BsWallet /> Nº:
-          </h3>
-          <p>{imovel[0].numero_imovel}</p>
-        </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Qtde quartos:
           </h3>
           <p>{imovel[0].qtdquartos_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Possui piscina?:
           </h3>
           <p>{imovel[0].piscina_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Possui garagem?:
           </h3>
           <p>{imovel[0].garagem_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Qtde carros na garagem?:
           </h3>
           <p>{imovel[0].qtdcarros_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info'>
+          <h3>
             <BsWallet /> Possui aréa de lazer?:
           </h3>
           <p>{imovel[0].area_de_lazer_imovel}</p>
         </div>
-        <div>
-          <h3 className='bairro'>
+        <div className='info description'>
+          <h3>
             <BsWallet /> Observações:
           </h3>
           <p>{imovel[0].observacoes_imovel}</p>
